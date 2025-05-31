a6a1c8bb49abaffdd644f0419c2670f81d8da8de
saito4 <hiroto.saitou1201@gmail.com>
2025-06-01 04:19:48 +0900

    add controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store'])->name('post.store');
