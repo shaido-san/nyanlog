1f21399e35cbd698ba983e7fc8e392c0c1fe4965
saito4 <hiroto.saitou1201@gmail.com>
2025-06-17 15:08:25 +0900

    地図表示機能追加

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -20,14 +20,42 @@
         </div>
         <div>
             <label for="latitude">緯度:</label>
-            <input type="text" name="latitude" id="latitude">
+            <input type="text" name="latitude" id="latitude" readonly style="background-color: #f0f0f0;>
         </div>
         <div>
             <label for="longitude">経度:</label>
-            <input type="text" name="longitude" id="longitude">
+            <input type="text" name="longitude" id="longitude" readonly style="background-color: #f0f0f0;">
+        </div>
+        <div style="margin-top: 20px;">
+            <h3>地図で猫ちゃんの位置を決めてね！緯度と経度は位置を決めれば自動で決まるよ！</h3>
+            <div id="map" style="height: 400px;"></div>
         </div>
         <button type="submit">投稿する</button>
     </form>
     <a href="{{ route('posts.index') }}">投稿一覧ページに戻る</a>
 </body>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+<script>
+    var map = L.map('map').setView([35.6812, 139.7671], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+        maxZoom: 18,
+    }).addTo(map);
+
+    var marker;
+
+    map.on('click', function(e) {
+        var lat = e.latlng.lat;
+        var lng = e.latlng.lng;
+
+        if (marker) map.removeLayer(marker);
+
+        marker = L.marker([lat, lng]).addTo(map);
+
+        document.getElementById('latitude').value = lat;
+        document.getElementById('longitude').value = lng
+    });
+</script>
 </html>
